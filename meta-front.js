9c06401c65eadba4cb6e4de0d33d7502747ae5b6
Anup Bhandari <Anup.b@HOBB.COM>
2023-01-03 14:57:55 +0530

    Rendered client avatars in session

--- meta-front.js
+++ meta-front.js
@@ -6,6 +6,7 @@ const ASPECT_RATIO = 2
 const NEAR_PLANE = 0.1
 const FAR_PLANE = 1000
 const FRAME_RATE = 60
+const ENABLE_LIGHT_GIZMOS = false
 
 let front = new THREE.Vector3(0, 0, -1)
 let right = new THREE.Vector3(1, 0, 0)
@@ -18,18 +19,19 @@ let scene = new THREE.Scene()
 
 let light = new THREE.DirectionalLight(0xFFFFFF, 1)
 light.position.set(-1, 2, 4)
+light.castShadow = true
+light.shadow.mapSize.width = 512
+light.shadow.mapSize.height = 512
+light.shadow.camera.near = 0.5
+light.shadow.camera.far = 500
+
 scene.add(light)
 
-let color = 0x44aa88
-let material = new THREE.MeshPhongMaterial({color})
-
-let boxWidth = 100;
-let boxHeight = 0.1;
-let boxDepth = 100;
-let geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth)
-let floor = new THREE.Mesh(geometry, material)
-floor.position.set(0, -2, -50)
-scene.add(floor)
+if(ENABLE_LIGHT_GIZMOS)
+{
+    let helper = new THREE.CameraHelper(light.shadow.camera)
+    scene.add(helper)
+}
 
 let canvas = document.querySelector("canvas")
 canvas.addEventListener("mousedown", onMouseDown)
@@ -39,16 +41,10 @@ window.addEventListener("keydown", onKeyDown)
 window.addEventListener("keyup", onKeyUp)
 
 let renderer = new THREE.WebGLRenderer({canvas, alpha:true})
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
 renderer.render(scene, camera)
 setInterval(render_loop, 1000/FRAME_RATE)
-
-let keyMap = 
-{
-    w : false,
-    s : false,
-    a : false,
-    d : false,
-}
 
 function render_loop()
 {
@@ -89,6 +85,14 @@ function onMouseMove(event)
         right = cross(front, up)
         lastPos = { x: cursorPos.x, y: cursorPos.y }
     }
+}
+
+let keyMap = 
+{
+    w : false,
+    s : false,
+    a : false,
+    d : false,
 }
 
 function onKeyDown(event)
@@ -141,4 +145,42 @@ function updateCameraPosition()
         camera.position.y += right.y
         camera.position.z += right.z
     }
+}
+
+let sceneMap = new Map()
+
+function addToWorld(sceneobject)
+{
+    let material = new THREE.MeshPhongMaterial({color: sceneobject.color})
+    let geometry = new THREE.BoxGeometry(sceneobject.dimension.x, sceneobject.dimension.y, sceneobject.dimension.z)
+    let box = new THREE.Mesh(geometry, material)
+    box.position.set(sceneobject.position.x, sceneobject.position.y, sceneobject.position.z)
+    box.receiveShadow = true
+    box.castShadow = true
+    scene.add(box)
+    if (sceneobject.token >= 0)
+        sceneMap.set(sceneobject.token, box)
+}
+
+function removeFromWorld(sceneobject)
+{
+    let box = sceneMap.get(sceneobject.token)
+    scene.remove(box)
+    sceneMap.delete(sceneobject.token)
+}
+
+const ACTION_ADD = "add"
+const ACTION_DELETE = "delete"
+
+window.onload = () => {
+    setTimeout(()=>{
+        let webSocket = new WebSocket("ws://localhost:8080")
+        webSocket.onmessage = (e) => {
+            let sceneObject = JSON.parse(e.data)
+            if (sceneObject.actionType == ACTION_ADD)
+                addToWorld(sceneObject)
+            else if (sceneObject.actionType == ACTION_DELETE)
+                removeFromWorld(sceneObject)
+        }
+    }, 5000) 
 }
